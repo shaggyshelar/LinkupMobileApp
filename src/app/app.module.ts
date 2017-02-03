a0deb1df08a09e792835b3e0363707e8596c93c5
shaggyshelar <shaggy.shelar@gmail.com>
2017-02-03 18:39:16 +0530

    Changed the path of some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/dashboard/dashboard';
+
+// // Corporate
+// import { LogATicketPage } from '../pages/Corporate/log-a-ticket/log-a-ticket';
+// import { ConferenceBookingPage } from '../pages/Corporate/conference-booking/conference-booking';
+
+// // Timesheet
+// import { MyTimesheetPage } from '../pages/Timesheet/my-timesheet/my-timesheet';
+// import { EnterTimesheetPage } from '../pages/Timesheet/enter-timesheet/enter-timesheet';
+// import { ApproveTimesheetPage } from '../pages/Timesheet/approve-timesheet/approve-timesheet';
+// import { ApprovedTimesheetPage } from '../pages/Timesheet/approved-timesheet/approved-timesheet';
+// import { TimesheetReportPage } from '../pages/Timesheet/timesheet-report/timesheet-report';
+// import { BiometricDiscrepancyApprovalPage } from '../pages/Timesheet/biometric-discrepancy-approval/biometric-discrepancy-approval';
+
 import { Auth } from '../providers/auth';
 
 @NgModule({
