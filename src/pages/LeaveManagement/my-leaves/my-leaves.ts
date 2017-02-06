9973430b3eae15ce34c3191493d2ecb578a143de
shaggyshelar <shaggy.shelar@gmail.com>
2017-02-06 21:32:46 +0530

    Completed my leaves grid display

--- src/pages/LeaveManagement/my-leaves/my-leaves.ts
+++ src/pages/LeaveManagement/my-leaves/my-leaves.ts
@@ -4,7 +4,7 @@ import { LeaveService } from '../index';
 import { Observable } from 'rxjs/Rx';
 import { Leave } from '../models/leave';
 import { LeaveDetail } from '../models/leaveDetail';
-
+import { AlertController, ItemSliding } from 'ionic-angular';
 
 @Component({
   selector: 'page-my-leaves',
@@ -14,7 +14,8 @@ export class MyLeavesPage {
   public leaveObs: Observable<Leave>;
   public leaveDetObs: Observable<LeaveDetail>;
   public leaveDetail: LeaveDetail;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public leaveService: LeaveService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public leaveService: LeaveService,
+    public alertCtrl: AlertController) {
 
   }
 
@@ -28,4 +29,23 @@ export class MyLeavesPage {
     console.log('ionViewDidLoad MyLeavesPage');
   }
 
+  goToLeaveDetail(leaveData: any) {
+    // go to the session detail page
+    // and pass in the session data
+    //this.navCtrl.push(SessionDetailPage, sessionData);
+  }
+
+  editLeave(slidingItem: ItemSliding, leaveData: any) {
+    let alert = this.alertCtrl.create({
+      title: 'Edit Leave',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          // close the sliding item
+          slidingItem.close();
+        }
+      }]
+    });
+    alert.present();
+  }
 }
