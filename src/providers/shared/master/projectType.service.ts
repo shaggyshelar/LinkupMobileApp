c44d3e08e8276a19255e10ed7eee3ae323c78ce8
shaggyshelar <shaggy.shelar@gmail.com>
2017-02-06 15:54:42 +0530

    Implemented leaveTypeMaster, priceType, priority, projectCategory, projectType, room, skillMaster services

---
+++ src/providers/shared/master/projectType.service.ts
@@ -0,0 +1,20 @@
+/** Angular Dependencies */
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+/** Module Level Dependencies */
+import { BaseService } from '../../index';
+
+/** Context for service calls */
+const CONTEXT = 'projectTypeMaster';
+
+/** Service Definition */
+@Injectable()
+export class ProjectTypeService extends BaseService {
+    constructor(public http: Http) {
+        super(http, CONTEXT);
+    }
+    getProjectTypes() {
+        return this.getList$(0, 0, true).map(res => res.json());
+    }
+}
