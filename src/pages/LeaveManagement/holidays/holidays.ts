21df22e399c83158a61f2fd88efe931c8d84e742
shaggyshelar <shaggy.shelar@gmail.com>
2017-02-22 21:38:19 +0530

    Integrated ngPrime

--- src/pages/LeaveManagement/holidays/holidays.ts
+++ src/pages/LeaveManagement/holidays/holidays.ts
@@ -12,8 +12,33 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'holidays.html'
 })
 export class HolidaysPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  events: any[];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.events = [
+      {
+        "title": "All Day Event",
+        "start": "2016-01-01"
+      },
+      {
+        "title": "Long Event",
+        "start": "2016-01-07",
+        "end": "2016-01-10"
+      },
+      {
+        "title": "Repeating Event",
+        "start": "2016-01-09T16:00:00"
+      },
+      {
+        "title": "Repeating Event",
+        "start": "2016-01-16T16:00:00"
+      },
+      {
+        "title": "Conference",
+        "start": "2016-01-11",
+        "end": "2016-01-13"
+      }
+    ];
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HolidaysPage');
