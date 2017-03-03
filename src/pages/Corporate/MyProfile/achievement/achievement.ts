a6f196d8acf875e514f53076c165a9f4b57454d6
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-03 19:21:38 +0530

    achievements page

--- src/pages/Corporate/MyProfile/achievement/achievement.ts
+++ src/pages/Corporate/MyProfile/achievement/achievement.ts
@@ -1,22 +1,73 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-/*
-  Generated class for the Achievement page.
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-achievement',
   templateUrl: 'achievement.html'
 })
 export class AchievementPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  achievements: any[] = ['winner', 'hacker', 'dark master'];
+  showAddEditView: Boolean = false;
+  isEditable: Boolean = false;
+  achievement: String = '';
+
+  achievementForm: FormGroup;
+
+  constructor(public navCtrl: NavController
+    , public navParams: NavParams
+    , public viewCtrl: ViewController
+    , public formBuilder: FormBuilder
+  ) {
+    this.achievementForm = formBuilder.group({
+      achievement: ['', Validators.compose([Validators.minLength(3), Validators.required])]
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AchievementPage');
+  }
+
+  addAchievement() {
+    this.showAddEditView = !this.showAddEditView;
+  }
+
+  close() {
+    this.viewCtrl.dismiss({ message: 'Closed achievement' });
+  }
+
+  cancelTapped() {
+    this.achievement = '';
+    this.addAchievement();
+    this.editAchievement();
+  }
+
+  submitTapped(item) {
+    /** Pushing to array temporarily */
+    if (this.achievementForm.valid) {
+      console.log('submit value => ',item.achievement);
+      this.achievements.push(item.achievement);
+      this.addAchievement();
+      this.editAchievement();
+    }
+
+    /**
+     * TODO: Make API call for add/edit
+     */
+  }
+
+  listItemTapped(item) {
+    if (this.isEditable) {
+      this.achievement = item;
+      // this.addAchievement();
+      this.showAddEditView = true;
+    }
+  }
+
+  editAchievement() {
+    this.isEditable = true;
   }
 
 }
