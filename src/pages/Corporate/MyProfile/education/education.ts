713443db98e8d58c2c52a120c1d0f6ef1dc301e8
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-03 19:21:48 +0530

    education page

--- src/pages/Corporate/MyProfile/education/education.ts
+++ src/pages/Corporate/MyProfile/education/education.ts
@@ -13,10 +13,50 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EducationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  isDisabled: Boolean = false;
+  showDetails: Boolean = false;
+  selectedItem: any = {};
+  education: any[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.education = [
+      {
+        canEdit: false,
+        class: 'Degree',
+        degree: 'Batchelor of Engineering',
+        grade: 'First Class',
+        yearOfPassing: 2016,
+        certificate: 'filename.jpg',
+        status: 'Approved',
+        hrComments: 'Ok'
+      },
+      {
+        canEdit: false,
+        class: 'Diploma',
+        degree: 'Batchelor of Engineering',
+        grade: 'First Class',
+        yearOfPassing: 2013,
+        certificate: 'filename.jpg',
+        status: 'Approved',
+        hrComments: 'Ok'
+      }
+    ];
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EducationPage');
+  }
+
+  addEducation() {
+    /** Add Button Clicked */
+  }
+
+  viewEditEducation(item) {
+    if (item) {
+      item.status != 'Approved' ? this.isDisabled = true : this.isDisabled = false;
+      this.showDetails = true;
+      this.selectedItem = item;
+    }
   }
 
 }
