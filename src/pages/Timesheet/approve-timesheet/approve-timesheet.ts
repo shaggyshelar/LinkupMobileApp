d8f33d67226b8fdc3d7dcbd1f30fa69f99d9131c
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-07 20:32:58 +0530

    ion-list added

--- src/pages/Timesheet/approve-timesheet/approve-timesheet.ts
+++ src/pages/Timesheet/approve-timesheet/approve-timesheet.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { TimesheetDetailsPage } from '../timesheet-details/timesheet-details';
+
 /*
   Generated class for the ApproveTimesheet page.
 
@@ -13,10 +15,46 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ApproveTimesheetPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  timeRec : any[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.timeRec = [
+      {
+        ApproverUser: 'Kunal Adhikari',
+        StartDate: '06/03/2017',
+        EndDate: '12/03/2017',
+        BillableHours:'40:0',
+        NonBillableHours:'0:0',
+        Status:'Approved',
+        SubmissionDate: '10/03/2017'
+      },
+      {
+        ApproverUser: 'Kunal Adhikari',
+        StartDate: '13/03/2017',
+        EndDate: '19/03/2017',
+        BillableHours:'40:0',
+        NonBillableHours:'0:0',
+        Status:'Pending',
+        SubmissionDate: '18/03/2017'
+      },
+      {
+        ApproverUser: 'Kunal Adhikari',
+        StartDate: '20/03/2017',
+        EndDate: '26/03/2017',
+        BillableHours:'40:0',
+        NonBillableHours:'0:0',
+        Status:'Rejected',
+        SubmissionDate: '18/03/2017'
+      },
+    ];
+   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ApproveTimesheetPage');
+  }
+
+  itemTapped(entry) {
+    this.navCtrl.push(TimesheetDetailsPage, entry);
   }
 
 }
