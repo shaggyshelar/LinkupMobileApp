29bf9917e956c92756f11be02a381ffff28e9d33
Mahesh Nikate <maheshnikate@Mahesh-MAC.local>
2017-03-08 11:32:14 +0530

    fixed conflicts

--- src/providers/shared/index.ts
+++ src/providers/shared/index.ts
@@ -1,8 +1,6 @@
 export * from './services/api.service';
 export * from './services/base.service';
 export * from './services/message.service';
-<<<<<<< HEAD
 export * from './services/spinner.service';
-=======
 export * from './services/common.service';
->>>>>>> develop
+
