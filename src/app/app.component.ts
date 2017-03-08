631f25c581de8167fdd331a74e6214ebd2271ce4
shaggyshelar <shaggy.shelar@gmail.com>
2017-03-08 11:09:01 +0530

    Implemented User details in sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,8 +64,10 @@ export class MyApp {
   corporatePages: PageInterface[] = [];
   certificationPages: PageInterface[] = [];
   projectsPages: PageInterface[] = [];
-
-  constructor(public platform: Platform, public auth: AuthService, public loadingCtrl: LoadingController) {
+  userDetail:any;
+  profileImageSrc:any;
+  
+constructor(public platform: Platform, public auth: AuthService, public loadingCtrl: LoadingController) {
     this.initializeApp();
 
     //this.activePage = this.pages[0];
@@ -79,6 +81,7 @@ export class MyApp {
           this.toggleCertificationMenu();
           this.toggleCorporateMenu();
           this.toggleProjectsMenu();
+          this.loadUserDetails();
         } else {
           this.isAuthenticated = false;
           this.rootPage = LoginPage;
@@ -90,11 +93,22 @@ export class MyApp {
     this.auth.logout();
   }
 
+  loadUserDetails() : void {
+    this.userDetail = this.auth.getCurrentUser();
+    console.error('Got User',this.userDetail);
+    if (this.userDetail && this.userDetail.ProfilePictureName) {
+      this.profileImageSrc = 'http://192.168.100.153:202/Profile%20Picture%20Library/' + this.userDetail.ProfilePictureName + '.JPG';
+    } else {
+      this.profileImageSrc = 'assets/img/default-user.jpg';
+    }
+  }
+
   initializeApp() {
     this.presentLoading();
     this.isAuthenticated = this.auth.isAuthenticated();
     if (this.auth.isAuthenticated()) {
       this.rootPage = HomePage;
+      this.loadUserDetails();
     } else {
       this.rootPage = LoginPage;
     }
