95590a169ae4baa03d6d6cb26b87e77aead0b69c
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-09 11:35:24 +0530

    DailyTimesheetDetailPage validations

--- src/pages/Timesheet/daily-timesheet-detail/daily-timesheet-detail.ts
+++ src/pages/Timesheet/daily-timesheet-detail/daily-timesheet-detail.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the DailyTimesheetDetail page.
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-daily-timesheet-detail',
   templateUrl: 'daily-timesheet-detail.html'
@@ -14,8 +10,20 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DailyTimesheetDetailPage {
 
   dailyData : any = {};
+  dailyTimesheetForm : FormGroup;
+  isViewModeBillable : Boolean = true;
+  isViewModeNonBillable : Boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams
+  , private formBuilder: FormBuilder
+  ) {
+    this.dailyTimesheetForm = this.formBuilder.group({
+      billableHours:['',Validators.compose([Validators.required])],
+      noteForBillableHours:['',Validators.compose([Validators.required])],
+      nonBillableHours:['',Validators.compose([Validators.required])],
+      noteForNonBillableHours:['',Validators.compose([Validators.required])],
+    });
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DailyTimesheetDetailPage');
