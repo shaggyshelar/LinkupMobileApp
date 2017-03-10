11a4824f4e9384c892705c525c4a1724650759e1
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-10 11:59:46 +0530

    changes according to the caller

--- src/pages/Timesheet/daily-timesheet-detail/daily-timesheet-detail.ts
+++ src/pages/Timesheet/daily-timesheet-detail/daily-timesheet-detail.ts
@@ -3,13 +3,15 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { DailyTimesheet } from '../models/daily-timesheet.model';
+
 @Component({
   selector: 'page-daily-timesheet-detail',
   templateUrl: 'daily-timesheet-detail.html'
 })
 export class DailyTimesheetDetailPage {
 
-  dailyData : any = {};
+  dailyData : DailyTimesheet;
   dailyTimesheetForm : FormGroup;
   isViewModeBillable : Boolean = true;
   isViewModeNonBillable : Boolean = false;
@@ -18,15 +20,26 @@ export class DailyTimesheetDetailPage {
   , private formBuilder: FormBuilder
   ) {
     this.dailyTimesheetForm = this.formBuilder.group({
-      billableHours:['',Validators.compose([Validators.required])],
-      noteForBillableHours:['',Validators.compose([Validators.required])],
-      nonBillableHours:['',Validators.compose([Validators.required])],
-      noteForNonBillableHours:['',Validators.compose([Validators.required])],
+      billableHours:[{value: this.navParams.data.readOnly ? this.navParams.data.dailyData.BillableHours :'' , disabled: this.navParams.data.readOnly}],
+      noteForBillableHours:[{value: this.navParams.data.readOnly ? this.navParams.data.dailyData.NoteForBillableHours :'' , disabled: this.navParams.data.readOnly}],
+      nonBillableHours:[{value: this.navParams.data.readOnly ? this.navParams.data.dailyData.NonBillableHours :'' , disabled: this.navParams.data.readOnly}],
+      noteForNonBillableHours:[{value: this.navParams.data.readOnly ? this.navParams.data.dailyData.NoteForNonBillableHours :'' , disabled: this.navParams.data.readOnly}],
     });
   }
 
   ionViewDidLoad() {
-    this.dailyData = this.navParams.data;
+    this.navParams.data.readOnly ? this.viewMyTimesheetMode() : this.enterTimesheetMode();
+  }
+
+  viewMyTimesheetMode() {
+    this.dailyData = this.navParams.data.dailyData;
+    this.isViewModeBillable = this.isViewModeNonBillable = this.navParams.data.readOnly;
+    console.log('viewMyTimesheetMode');
+  }
+
+  enterTimesheetMode() {
+    console.log('enterTimesheetMode');
+    this.dailyData.BillableHours = this.dailyData.NonBillableHours = this.dailyData.NoteForBillableHours = this.dailyData.NoteForNonBillableHours = '';
   }
 
 }
