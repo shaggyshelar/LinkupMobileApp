50fea9d325b4cf443ac27f9199d7ee1d10e9c23d
shaggyshelar <shaggy.shelar@gmail.com>
2017-03-21 12:42:24 +0530

    Implemented scrollable header

--- src/components/expandable-header/expandable-header.ts
+++ src/components/expandable-header/expandable-header.ts
@@ -1,16 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, Renderer } from '@angular/core';
 
 @Component({
   selector: 'expandable-header',
   templateUrl: 'expandable-header.html'
 })
 export class ExpandableHeaderComponent {
+  @Input('scrollArea') scrollArea: any;
+  headerHeight: any;
+  newHeaderHeight: any;
 
-  text: string;
+  constructor(public element: ElementRef, public renderer: Renderer) {
+    this.headerHeight = 150;
+    this.renderer.setElementStyle(this.element.nativeElement, 'height', this.headerHeight + 'px');
+  }
 
-  constructor() {
-    console.log('Hello ExpandableHeader Component');
-    this.text = 'Hello World';
+  ngAfterViewInit() {
+    this.scrollArea.ionScroll.subscribe((ev) => {
+      this.resizeHeader(ev);
+    });
+  }
+
+  resizeHeader(ev) {
+    ev.domWrite(() => {
+      //console.log(ev.scrollTop);
+      this.newHeaderHeight = this.headerHeight - ev.scrollTop;
+      console.log('New=' + this.newHeaderHeight + ', top=' + ev.scrollTop);
+      if (this.newHeaderHeight < 0) {
+        this.newHeaderHeight = 0;
+        //this.renderer.setElementStyle(this.element.nativeElement, 'margin-top', this.newHeaderHeight + 'px');
+      }
+      this.renderer.setElementStyle(this.element.nativeElement, 'height', this.newHeaderHeight + 'px');
+    });
   }
 
 }
