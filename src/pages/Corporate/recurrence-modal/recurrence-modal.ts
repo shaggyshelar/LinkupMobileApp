05fa8129c36e4119f0e1051ff15ecf977e953da7
mukulvtilak <tilak.mukul@hotmail.com>
2017-03-24 20:22:42 +0530

    recurrence modal UI

--- src/pages/Corporate/recurrence-modal/recurrence-modal.ts
+++ src/pages/Corporate/recurrence-modal/recurrence-modal.ts
@@ -12,8 +12,33 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
   templateUrl: 'recurrence-modal.html'
 })
 export class RecurrenceModalPage {
-
+  /** Get with Master API call */
+  daysRepeat: any;
   pattern: any[];
+  days: any[];
+  daysP2: any[];
+  weekCount: any[];
+
+  repeatDay: any;
+  allWeekdays: boolean;
+  isRepeatWithDay: boolean = true;
+  dayP2: any;
+  isEventEnding: boolean = true;
+  isRepeating: boolean = false;
+  isEndAfterOccurences: boolean = true;
+  occurences: any;
+  eventStrtDt: any;
+  eventEndDt: any;
+  eventOccurences: number;
+  months:any[];
+  repeatingDays: any[];
+  dayOfMonth: any;
+  recurAfterMonth: any;
+  weekNum: any;
+  yearlyMonthRecur: any;
+  yearlyDayDateRecur: any;
+  yearlyWeekRecue: any;
+  yearlyDayRecur: any;
   constructor(public navCtrl: NavController, public navParams: NavParams
   , public viewCtrl: ViewController
   ) {
@@ -23,6 +48,37 @@ export class RecurrenceModalPage {
       'Monthly',
       'Yearly'
     ];
+    this.days = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ];
+    this.months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    this.weekCount= [
+      'First',
+      'Second',
+      'Third',
+      'Fourth',
+      'Last',
+    ];
+    this.allWeekdays = true;
   }
 
   ionViewDidLoad() {
@@ -33,4 +89,19 @@ export class RecurrenceModalPage {
     this.viewCtrl.dismiss({data : null});
   }
 
+  radioChnage(repeat) {
+    console.log(repeat);
+  }
+
+  eventRepeatingChanged() { }
+
+  dailyRepeatingChanged() { }
+
+  monthlyRepeatingChanged() { }
+
+  yearlyRepeatingChanged() { }
+
+  endChanged() { }
+
+  endValuesChanged() { }
 }
