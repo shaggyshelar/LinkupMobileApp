906447b113f8ecd2d54e9c3449f9e35423645506
mukulvtilak <tilak.mukul@hotmail.com>
2017-04-17 14:35:24 +0530

    fab button for add new project

--- src/pages/Projects/manage-my-projects/manage-my-projects.ts
+++ src/pages/Projects/manage-my-projects/manage-my-projects.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ProjectService } from '../index';
-import { EmployeeProjectManagementPage } from '../employee-project-management/employee-project-management';
+import { AddProjectsPage } from '../add-projects/add-projects';
 import { Project } from '../models/project';
 import { SpinnerService } from '../../../providers/index';
+import { AuthService } from '../../../providers/index';
 /** Component Declaration */
 import { Observable } from 'rxjs/Rx';
 
@@ -16,38 +17,42 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'page-manage-my-projects',
   templateUrl: 'manage-my-projects.html',
-  providers:[ProjectService,SpinnerService]
+  providers: [ProjectService, SpinnerService]
 })
 export class ManageMyProjectsPage {
-
-    projectList: Observable<Project[]>;
-  constructor(public navCtrl: NavController, 
-  public navParams: NavParams,
-   private projectService: ProjectService,
-   private spinnerService:SpinnerService) { 
-   
-   }
+  isAuthorized: boolean;
+  projectList: Observable<Project[]>;
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private projectService: ProjectService,
+    private spinnerService: SpinnerService,
+    public auth: AuthService,
+  ) {
+    this.isAuthorized = this.auth.checkPermission('PROJECTS.EMPLOYEEPROJECTMANAGEMENT.ADD')
+  }
 
   ionViewDidLoad() {
     //console.log('ionViewDidLoad ManageMyProjectsPage');
     this.getActiveProjects();
   }
-  getActiveProjects()
-  {
-     //this.projectList = this.projectService.getProjectList();
-      this.spinnerService.createSpinner('Please wait');
-      this.projectService.getProjectList().subscribe(
+  getActiveProjects() {
+    //this.projectList = this.projectService.getProjectList();
+    this.spinnerService.createSpinner('Please wait');
+    this.projectService.getProjectList().subscribe(
       (res: any) => {
         this.spinnerService.stopSpinner();
-       this.projectList = res;   
-      },error=>{
-       this.spinnerService.stopSpinner();
+        this.projectList = res;
+      }, error => {
+        this.spinnerService.stopSpinner();
       });
-     
+
   }
-  goToProjectDetail(project:any)
-  {
-     this.navCtrl.push(EmployeeProjectManagementPage, { selectedProject: project});
+  goToProjectDetail(project: any) {
+    this.navCtrl.push(AddProjectsPage, { selectedProject: project });
+  }
+
+  addFabClicked() {
+    this.navCtrl.push(AddProjectsPage);
   }
 
 }
