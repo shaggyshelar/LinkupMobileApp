efbd63f9e27fb64a5c3e5177a4257cb4321695a7
mukulvtilak <tilak.mukul@hotmail.com>
2017-05-02 20:17:40 +0530

    maxdate validatn,discrepancy for range of date,leave validation

--- src/pages/discrepancy-modal/discrepancy-modal.ts
+++ src/pages/discrepancy-modal/discrepancy-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { NavController, NavParams, ViewController, ToastController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import * as moment from 'moment';
@@ -8,13 +8,9 @@ import { MessageService, SpinnerService, EmployeeDiscrepancyService, AuthService
 
 import { ApplyForLeavePage } from '../LeaveManagement/apply-for-leave/apply-for-leave';
 import { Discrepancy } from './discrepancy';
+import { LeaveService } from '../LeaveManagement/services/leave.service';
+import { LeaveTypeMasterService } from '../../providers/shared/master/leaveTypeMaster.service';
 
-/*
-  Generated class for the DiscrepancyModal page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-discrepancy-modal',
   templateUrl: 'discrepancy-modal.html'
@@ -23,19 +19,25 @@ export class DiscrepancyModalPage {
   userDetail: any;
   discrepancyData: any;
   biometricReasons: any[];
+  leaveTypes: any[] = [];
   showReason: boolean;
   showDateRange: boolean;
   wasLeaveTaken: boolean;
   data: Discrepancy;
   complexForm: FormGroup;
   minDate: any;
+  maxDate: any;
+  reasonID: number = 999;
   constructor(public navCtrl: NavController, public navParams: NavParams
     , public discrepancyService: EmployeeDiscrepancyService
     , public fb: FormBuilder
     , public auth: AuthService
+    , public leaveService: LeaveService
+    , public leaveMaster: LeaveTypeMasterService
     , public viewCtrl: ViewController
+    , private toastCtrl: ToastController
   ) {
-    this.discrepancyData = {};
+    this.discrepancyData = [];
     this.biometricReasons = [];
     this.showReason = false;
     this.showDateRange = false;
@@ -47,24 +49,72 @@ export class DiscrepancyModalPage {
       'To': []
     });
     this.userDetail = this.auth.getCurrentUser();
-    this.discrepancyData = this.navParams.data;
-    console.log('discrepancyData => ', this.discrepancyData);
+    var data = this.navParams.data;
+    data.forEach(element => {
+      this.discrepancyData.push(element);
+    });
+    this.checkIfAlreadyApplied();
+    // console.log('discrepancyData => ', this.discrepancyData);
     this.getReasonsMaster();
-    this.initModel();
+    // this.initModel();
   }
 
-  ionViewDidLoad() {
+  ionViewDidLoad() { }
 
+  getTypeofLeaves() {
+    this.leaveMaster.getLeaveTypes().subscribe((res) => {
+      res.forEach(element => {
+        this.leaveTypes.push({ Id: res.ID, Value: res.Name });
+      });
+    });
+  }
+
+  checkIfAlreadyApplied() {
+    var discrepancyDates = [];
+    var discrepancyIndex = [];
+    var spliced = []
+    this.discrepancyData.forEach((element, index) => {
+      this.leaveService.getCurrentUserLeaveDiscrepancy({ Date: element.LeaveDate }).subscribe(res => {
+        if (res.length > 0)
+          discrepancyDates = res;
+        console.log('Date : ' + element.LeaveDate + ' =>', res);
+      });
+    });
+
+    if (discrepancyDates.length > 0)
+      this.discrepancyData.forEach((element, index) => {
+        discrepancyDates.forEach((item) => {
+          if (element.LeaveDate == item.StartDate) {
+            discrepancyIndex.push(index);
+          }
+        });
+      });
+
+    if (discrepancyIndex.length > 0) {
+      discrepancyIndex.forEach(item => {
+        var splice = this.discrepancyData.splice(item, 1);
+        if(splice.length > 0)
+          splice.forEach(element => {
+            spliced.push(element);
+          });
+      });
+    }
+
+    // TODO : Make API call for each entry's contents in spliced[]
   }
 
   initModel() {
-    // this.data = {
-    //   ID: 0,
-    //   Employee: { Name: '', ID: 0 },
-    //   SpecifyReason: '',
-    //   From: '',
-    //   To: ''
-    // };
+    return {
+      ID: 0,
+      Employee: { Name: '', ID: 0 },
+      Approvers: [],
+      Department: '',
+      LeaveDate: '',
+      LeaveReason: '',
+      ReasonDetails: '',
+      EmployeeID: 0,
+      ApproverStatus: ''
+    };
   }
 
   getReasonsMaster() {
@@ -75,27 +125,75 @@ export class DiscrepancyModalPage {
 
   }
 
+  submitRangedPayload(formValue) {
+    let toForm = moment(formValue.To).add(1, 'day');
+    let fromForm = moment(formValue.From);
+    let dayDiff = moment(toForm).diff(fromForm, 'days');
+    let payload: Discrepancy[] = [];
+    for (var index = 0; index <= dayDiff; index++) {
+      payload.push({
+        ID: this.discrepancyData[index].ID,
+        Employee: this.userDetail.Employee,
+        Approvers: [],
+        Department: this.userDetail.Department.Value,
+        LeaveDate: this.discrepancyData[index].LeaveDate,
+        LeaveReason: formValue.Reason,
+        ReasonDetails: formValue.SpecifyReason,
+        EmployeeID: this.userDetail.EmpID,
+        ApproverStatus: ''
+      });
+    }
+
+    return payload;
+  }
+
   submitPayload(formValue) {
-    // this.data.Employee = this.userDetail.Employee;
-    // this.data.SpecifyReason = formValue.SpecifyReason;
-    // this.data.From = moment(formValue.From).toISOString();
-    // this.data.To = moment(formValue.To).toISOString();
-    // this.data.ID = this.discrepancyData.ID;
-    // console.log('discrepancy payload =>', this.data);
+    let payload: Discrepancy = this.initModel();
+    payload.ID = this.discrepancyData[0].ID;
+    payload.Employee = this.userDetail.Employee;
+    payload.Approvers = [];
+    payload.Department = this.userDetail.Department.Value;
+    payload.LeaveDate = this.discrepancyData[0].LeaveDate;
+    payload.LeaveReason = formValue.Reason;
+    payload.ReasonDetails = formValue.SpecifyReason;
+    payload.EmployeeID = this.userDetail.EmpID;
+    return payload;
   }
 
   submitClick(formValue) {
+    console.log('form value => ', formValue)
     if (this.wasLeaveTaken) {
-      // this.navCtrl.push(ApplyForLeavePage, { date: this.discrepancyData.LeaveDate });
-      // this.wasLeaveTaken = false;
       this.viewCtrl.dismiss({
-        date: this.discrepancyData.LeaveDate,
+        date: this.discrepancyData[0].LeaveDate,
         wasLeaveTaken: this.wasLeaveTaken
       });
-    } else {
+    }
+    else {
       this.submitPayload(formValue);
-      // POST API Call
-      // this.navCtrl.pop();
+      if (this.reasonID == 1 || this.reasonID == 3 || this.reasonID == 5) {
+        var errMsg, okFlag;
+        var payload = this.submitRangedPayload(formValue);
+
+        payload.forEach((entry, index) => {
+          this.discrepancyService.updateEmployeeDiscrepancy(entry).subscribe(res => {
+            res.StatusCode == 1 ? this.toastPresent('Entry submitted successfully') : this.toastPresent(res.Message);
+          });
+        });
+
+      }
+      if (this.reasonID == 6) {
+
+        this.discrepancyService.updateEmployeeDiscrepancy(this.submitPayload(formValue)).subscribe(res => {
+          if (res.StatusCode == 1) {
+            this.toastPresent('Entry submitted successfully');
+          } else {
+            this.toastPresent(res.Message);
+          }
+        }, err => {
+          console.log(err);
+        });
+
+      }
       this.viewCtrl.dismiss({
         date: null,
         wasLeaveTaken: this.wasLeaveTaken
@@ -116,10 +214,12 @@ export class DiscrepancyModalPage {
   }
 
   updateView(filtered) {
+    this.reasonID = filtered.ID;
     switch (filtered.ID) {
       case 1:   //WFH
         this.showDateRange = true;
         this.showReason = false;
+        this.wasLeaveTaken = false;
         break;
 
       case 2:   //Leave
@@ -131,16 +231,19 @@ export class DiscrepancyModalPage {
       case 3:   //Clientside
         this.showReason = false;
         this.showDateRange = true;
+        this.wasLeaveTaken = false;
         break;
 
       case 5:   //Out of Office Duty
         this.showReason = true;
         this.showDateRange = true;
+        this.wasLeaveTaken = false;
         break;
 
       case 6:   //Other
         this.showReason = true;
         this.showDateRange = false;
+        this.wasLeaveTaken = false;
         break;
     }
   }
@@ -149,14 +252,25 @@ export class DiscrepancyModalPage {
     this.complexForm = this.fb.group({
       'Reason': [reasonValue, Validators.required],
       'SpecifyReason': [null, this.showReason ? Validators.required : null],
-      'From': [null, this.showDateRange ? Validators.required : null],
+      'From': [this.discrepancyData[0].LeaveDate, this.showDateRange ? Validators.required : null],
       'To': [null, this.showDateRange ? Validators.required : null]
     });
+    this.minDate = moment(this.discrepancyData[0].LeaveDate).format('YYYY-MM-DD');
+    this.maxDate = moment(this.minDate).add(5, 'years').format('YYYY-MM-DD');
   }
 
   fromDateChanged(event) {
-    this.minDate = moment(event.month.text + '/' + event.day.text + '/' + event.year.text).toISOString();
+    this.minDate = moment(event.month.text + '/' + event.day.text + '/' + event.year.text).add(1, 'days').toISOString();
+    this.maxDate = moment(this.minDate).add(5, 'years').toISOString();
     console.log('from date change => ', this.minDate);
+  }
+
+  toastPresent(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 5000
+    });
+    toast.present();
   }
 
 }
