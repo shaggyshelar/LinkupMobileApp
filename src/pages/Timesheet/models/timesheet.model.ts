5471cac03056f56023f1bb5da97663a655e17311
mukulvtilak <tilak.mukul@hotmail.com>
2017-05-23 18:04:40 +0530

    new timesheet status changed

--- src/pages/Timesheet/models/timesheet.model.ts
+++ src/pages/Timesheet/models/timesheet.model.ts
@@ -31,7 +31,7 @@ export class Timesheet {
         Saturdaynbhrs: string = null;
         Sundaynbhrs: string = null;
         WeekNumber: string = null;
-        ProjectTimesheetStatus: string = 'New';
+        ProjectTimesheetStatus: string = 'New Timesheet';
         StartDate: string = null;
         EndDate: string = null;
         Mondaydescnb: string = null;
